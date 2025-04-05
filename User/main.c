12dbc3aed396c8d9d5a4b1b28dad643f86ae0438
levon24 <levon24@mail.ru>
2025-04-05 11:27:24 +0400

    Updated format

--- User/main.c
+++ User/main.c
@@ -11,15 +11,14 @@
  *******************************************************************************/
 
 /*
- *@Note
- *Multiprocessor communication mode routine:
- *Master:USART1_Tx(PD5)\USART1_Rx(PD6).
- *This routine demonstrates that USART1 receives the data sent by CH341 and inverts
- *it and sends it (baud rate 115200).
+ * @Note
+ * Multiprocessor communication mode routine:
+ * Master: USART1_Tx(PD5)\USART1_Rx(PD6).
+ * This routine demonstrates that USART1 receives the data sent by CH341 and inverts
+ * it and sends it (baud rate 115200).
  *
- *Hardware connection:PD5 -- Rx
- *                     PD6 -- Tx
- *
+ * Hardware connection: PD5 -- Rx
+ *                      PD6 -- Tx
  */
 
 #include "debug.h"
@@ -38,31 +37,30 @@ vu8 val;
  *
  * @return  none
  */
-void USARTx_CFG(void)
-{
-    GPIO_InitTypeDef  GPIO_InitStructure = {0};
-    USART_InitTypeDef USART_InitStructure = {0};
+void USARTx_CFG(void) {
+  GPIO_InitTypeDef  GPIO_InitStructure = {0};
+  USART_InitTypeDef USART_InitStructure = {0};
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1, ENABLE);
 
-    /* USART1 TX-->D.5   RX-->D.6 */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
+  /* USART1 TX-->D.5   RX-->D.6 */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-    USART_InitStructure.USART_BaudRate = 115200;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+  USART_InitStructure.USART_BaudRate = 115200;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
 
-    USART_Init(USART1, &USART_InitStructure);
-    USART_Cmd(USART1, ENABLE);
+  USART_Init(USART1, &USART_InitStructure);
+  USART_Cmd(USART1, ENABLE);
 }
 
 /*********************************************************************
@@ -72,33 +70,28 @@ void USARTx_CFG(void)
  *
  * @return  none
  */
-int main(void)
-{
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-    SystemCoreClockUpdate();
-    Delay_Init();
+int main(void) {
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+  SystemCoreClockUpdate();
+  Delay_Init();
 #if (SDI_PRINT == SDI_PR_OPEN)
-    SDI_Printf_Enable();
+  SDI_Printf_Enable();
 #else
-    USART_Printf_Init(115200);
+  USART_Printf_Init(115200);
 #endif
-    printf("SystemClk:%d\r\n",SystemCoreClock);
-    printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+  printf("SystemClk:%d\r\n",SystemCoreClock);
+  printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
 
-    USARTx_CFG();
+  USARTx_CFG();
 
-    while(1)
-    {
-
-        while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET)
-        {
-            /* waiting for receiving finish */
-        }
-        val = (USART_ReceiveData(USART1));
-        USART_SendData(USART1, ~val);
-        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
-        {
-            /* waiting for sending finish */
-        }
+  while(1) {
+    while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET) {
+      /* waiting for receiving finish */
     }
+    val = (USART_ReceiveData(USART1));
+    USART_SendData(USART1, ~val);
+    while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET) {
+      /* waiting for sending finish */
+    }
+  }
 }
